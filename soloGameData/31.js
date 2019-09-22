204365849fd38a7a648ea8abcb6b2bffb353f209
FEDORAS <william.gao09@gmail.com>
2019-09-22 18:16:38 -0400

    the circuit for 31 works; no win condition

--- soloGameData/31.js
+++ soloGameData/31.js
@@ -23,16 +23,24 @@ class Card {
     constructor(s, v) {
         this.suit = s;
         this.val = v;
-        this.fullame = s + " " +v
+        this.fullName = s + " " +v;
+    }
+    toString() {
+        return (this.suit +  " " + this.val);
     }
 }
 
 class Stack extends Array{
-    constructor() {
+    /**
+     * 
+     * @param {String} n Optional name of stack
+     */
+    constructor(n) {
         super();
+        this.fullName = n;
     }
     drawCard() {
-        return(this.shift);
+        return(this.shift());
     }
     /** @param {Card} card Card to add to the top of the stack */
     addToTop(card) {
@@ -41,7 +49,7 @@ class Stack extends Array{
     shuffle() {
         var tDeck = new Stack();
         while(this.length) {
-            var aCard = this.splice(drawRando(0,this.length - 1),1)[0];
+            var aCard = this.splice(moreMath.randInt(0,this.length - 1),1)[0];
             tDeck.push(aCard);
         }
         this.push.apply(this,tDeck);
@@ -65,6 +73,13 @@ class Stack extends Array{
             return(Math.max(cardsBySuit.DIA, cardsBySuit.CLB, cardsBySuit.HRT, cardsBySuit.SPD));
         }
 
+    }
+    toString() {
+        var nStr = `The ${this.fullName} consists of `;
+        for (var card in this) {
+            nStr += this[card].fullName + ", ";
+        }
+        return(nStr);
     }
 }
 
@@ -90,14 +105,37 @@ class Player {
     }
     /** @param {Stack} s Stack to draw from */
     drawFromStack(s) {
-        this.hand.push( s.drawCard() );
+        var someCard = s.drawCard();
+
+        this.hand.push( someCard );
+        console.log("Seat " + this.seat + " drew from " + s.fullName + ", drawing a " + someCard.toString());
     }
     /**
      * @param {Number} i Index for discarding card
-     * @param {*} s Stack to discard to
+     * @param {Stack} s Stack to discard to
      */
     discardToStack(i, s) {
-        s.addToTop( this.hand.splice(i,1) );
+        var someCard = this.hand.splice(i,1)[0];
+        console.log("Seat " + this.seat + " discarded a " + someCard.toString());
+        s.addToTop( someCard );
+    }
+    toString() {
+        var nStr = `Seat ${this.seat} currently has, from left to right, `;
+        for (var card in this.hand) {
+            if (typeof this.hand[card] == "object")
+                nStr += this.hand[card].fullName + ", ";
+        }
+        return(nStr);
+    }
+    /**
+     * 
+     * @param {Stack} ds discard stack
+     * @param {Stack} dw drawing stack
+     */
+    //yo wtf is happening here lol
+    static randomAct(ds, dw) {
+        this.drawFromStack(arguments[moreMath.randInt(0,1)]);
+        this.discardToStack(moreMath.randInt(0,3), ds);
     }
 }
 
@@ -110,30 +148,92 @@ class GameEnvironment {
     constructor(players, f) {
         this.players = players;
         this.focus = f;
+
+        this.winner = -1;
     }   
     //these are relative to their position on an array
     getNextLeft() {
-        f--;
-        if (f == -1) {
-            f = this.players.length - 1;
+        this.focus--;
+        if (this.focus == -1) {
+            this.focus = this.players.length - 1;
         }
     }
     getNextRight() {
-        f++;
-        if (f == this.players.length) {
-            f = 0;
+        this.focus++;
+        if (this.focus == this.players.length) {
+            this.focus = 0;
         }
     }
 
-    main() {
+    //THE MAIN METHOD (is buried in here)
+    //it executes the game, but do note that 
+    //THIS IS NOT "MAIN" MAIN
+    //THERE ARE CHUNKS OF INITIALIZING CODE AT THE BOTTOM
+    async main() {
+        var facedownDeck = new Stack("face down pile");
+        var discardPile = new Stack("discard pile");
+        const SUITS = ["DIA","CLB","HRT","SPD"];
+        for (var s in SUITS) {
+            for (var v = 1; v != 14; v++) {
+                facedownDeck.addToTop( new Card(SUITS[s], v) );
+            }
+        }
+        facedownDeck.shuffle();
+        discardPile.addToTop( facedownDeck.drawCard() ); //make sure that its legal to draw from disco at any time
+        var knocked = 1;
+        for (var plyr in this.players) {
+            for (var n = 0 ;n != 3; n++) {
+                this.players[plyr].drawFromStack(facedownDeck);
+            }
+        }
+        while (this.winner == -1) {
+            this.getNextRight();
+            if (this.players[this.focus].isBot) {
+                this.players[this.focus].botFunc(discardPile, facedownDeck);
+            } else {
+                console.log(this.players[this.focus].toString());
+                var drawPromise = new Promise(function(resolve, reject) {
+                    Input.get("Draw from facedown deck or discard pile? (F/D)", function(choice) {
+                        resolve(choice);
+                    });
+                });
+                var drawFrom = await drawPromise;
+                switch(drawFrom) {
+                    case("F"):
+                        this.players[this.focus].drawFromStack(facedownDeck);
+                        break;
+                    case("D"):
+                        this.players[this.focus].drawFromStack(discardPile);
+                        break;        
+                }
+                console.log(this.players[this.focus].toString());
+                var discardPromise = new Promise(function(resolve,reject){
+                    Input.get("Which card do you discard? (0/1/2/3)", function(choice) {
+                        resolve(parseInt(choice));
+                    });
+                });
+                var discoIndex = await discardPromise;
+                this.players[this.focus].discardToStack(discoIndex, discardPile);
+
+            }
+        }
+
+    }
+
+    playerHandler() {
 
     }
 }
 
-// Input.question("WHATS YOUR FACTORIVE APPLE", function(str) {
-//     console.log(str);
-//     Input.close();
-// })
+// var readline = require("readline");
+// var nInterface = readline.createInterface({
+//     input: process.stdin,
+//     output:process.stdout
+// });
+// nInterface.question("whats your favorite color?", function(nStr) {
+//     console.log(nStr);
+// });
+// console.log("ITS ASYNC");
 
 //is this really necessary? does this waste memory? the answers are likely no and yes
 class Input {
@@ -155,8 +255,31 @@ class Input {
     }
 }
 
-Input.get("WHATS YOUR FAVORITE NUMBER?", function(nStr){
-    console.log(nStr);
-});
-console.log("TEXT");
 
+
+// Input.get("WHATS YOUR FAVORITE NUMBER?", function(nStr){
+//     console.log(nStr);
+// });
+// console.log("TEXT");
+
+
+
+
+var playerList = [
+    new Player(0, false), 
+    new Player(1, true, Player.randomAct), 
+    new Player(2, true, Player.randomAct), 
+    new Player(3, true, Player.randomAct)
+];
+
+var dealer = moreMath.randInt(0,3);
+console.log("DEALRR " + dealer);
+
+var gameSession = new GameEnvironment(playerList, dealer);
+gameSession.main();
+
+// Input.get("whats your favorite color?", function(nStr) {
+//     console.log("this was suppoed to happen before");
+// });
+
+// console.log("after the inpuit get");
--- soloGameData/moreMath.js
+++ soloGameData/moreMath.js
@@ -1,5 +1,9 @@
 
-
+/**
+ * 
+ * @param {Number} low Inclusive required
+ * @param {Number} high Inclusive required
+ */
 exports.randInt = function(low, high) {
     var diff = (high - (low - 1));
     var rando = Math.ceil( Math.random() * diff) + (low - 1) ;
